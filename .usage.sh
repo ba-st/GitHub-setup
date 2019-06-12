f8c07765a3f1d8a4a9a98f36f393c6836b60dbb2
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2019-06-12 17:49:07 -0300

    Fixed typo

--- .usage.sh
+++ .usage.sh
@@ -7,14 +7,14 @@ usage() {
 
     cat << EOF
 Usage: $(basename $0) -p PROJECT_NAME [-r REPO_NAME] [-o OWNER] [-c COPYRIGHT_HOLDER] [-d DEFAULT_BRANCH]
-This program will create a project teamplate using ba-st conventions.
+This program will create a project template using ba-st conventions.
 Options:
--p PROJECT_NAME     Used for the name of the project
+-p PROJECT_NAME     Used for the name of the project.
 -r REPO_NAME        Used for the code repository.  Defaults to PROJECT_NAME.
 -o OWNER            Used as the repository owner. Defaults to ba-st.
 -c COPYRIGHT_HOLDER Used as the copyright holder in the license file. Defaults to Buenos Aires Smalltalk Contributors.
--d DEFAULT_BRANCH   Used as the default branch name in the documentation templates. Defaults to release-candidate
--h                  Display this usage message and exit
+-d DEFAULT_BRANCH   Used as the default branch name in the documentation templates. Defaults to release-candidate.
+-h                  Display this usage message and exit.
 
 Example:
     $(basename $0) -p "Willow Bootstrap" -r Willow-Bootstrap -o gcotelli -c "Monsters Inc." -d master
