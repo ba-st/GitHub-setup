4fd8d4d37b4fab3db32def751810722a33636a8e
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2020-04-30 17:47:37 -0300

    Add parameter to choose the build service to use: GitHub Actions or Travis CI.

--- setup.sh
+++ setup.sh
@@ -13,8 +13,10 @@ REPO_OPTION=""
 OWNER_OPTION=""
 COPYRIGHT_HOLDER_OPTION=""
 DEFAULT_BRANCH=""
+BUILD_SERVICE_OPTION=""
+
 # Parse command line options
-while getopts "p:r:o:c:d:h" option; do
+while getopts "p:r:o:c:d:b:h" option; do
   #shellcheck disable=SC2220
   case $option in
     p) PROJECT_OPTION=$OPTARG ;;
@@ -22,6 +24,7 @@ while getopts "p:r:o:c:d:h" option; do
     o) OWNER_OPTION=$OPTARG ;;
     c) COPYRIGHT_HOLDER_OPTION=$OPTARG ;;
     d) DEFAULT_BRANCH=$OPTARG ;;
+    b) BUILD_SERVICE_OPTION=$OPTARG ;;
     h) usage ;;
   esac
 done
@@ -35,6 +38,11 @@ readonly REPO_NAME="${REPO_OPTION:-$PROJECT_NAME}"
 readonly OWNER="${OWNER_OPTION:-ba-st}"
 readonly COPYRIGHT_HOLDER="${COPYRIGHT_HOLDER_OPTION:-"Buenos Aires Smalltalk Contributors"}"
 readonly DEFAULT_BRANCH="${DEFAULT_BRANCH:-release-candidate}"
+readonly BUILD_SERVICE="${BUILD_SERVICE_OPTION:-"github"}"
+
+if [ "${BUILD_SERVICE}" != "github" ] && [ "${BUILD_SERVICE}" != "travis" ]; then
+  usage "Invalid build service. Valid options are github or travis."
+fi
 
 print_notice "Creating template for $PROJECT_NAME hosted at https://github.com/$OWNER/$REPO_NAME"
 
@@ -77,7 +85,12 @@ cp templates/.project "$EXPORT_LOCATION/.project"
 cp templates/.gitattributes "$EXPORT_LOCATION/.gitattributes"
 print_success "  [OK]"
 
-print_info "  Copying GitHub Actions configuration..."
-cp -r templates/.github "$EXPORT_LOCATION/.github"
+if [ "${BUILD_SERVICE}" == "github" ]; then
+  print_info "  Copying GitHub Actions configuration..."
+  sed "$REPLACE_TEMPLATE_VARS" templates/.travis.yml > "$EXPORT_LOCATION/.travis.yml"
+elif [ ${BUILD_SERVICE} == "travis" ]; then
+  print_info "  Copying Travis CI configuration..."
+  cp -r templates/.github "$EXPORT_LOCATION/.github"
+fi
 sed "$REPLACE_TEMPLATE_VARS" templates/.smalltalk.ston > "$EXPORT_LOCATION/.smalltalk.ston"
 print_success "  [OK]"
